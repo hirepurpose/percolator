d93546fc628b6a03d4fc2c60a000d9a8eb3df334
Brian W. Wolter <brian@brianwolter.com>
2017-07-24 09:49:02 -0400

    ...

--- src/perc/service/service.go
+++ src/perc/service/service.go
@@ -36,7 +36,7 @@ func init() {
   metrics.Register("percolator.proxy.conn.error", proxyConnError)
   proxyXferError = metrics.NewMeter()
   metrics.Register("percolator.proxy.xfer.error", proxyXferError)
-  proxyResolveError = metrics.NewTimer()
+  proxyResolveError = metrics.NewMeter()
   metrics.Register("percolator.proxy.resolve.error", proxyResolveError)
   proxyResolveTimer = metrics.NewTimer()
   metrics.Register("percolator.proxy.resolve.latency", proxyResolveTimer)
