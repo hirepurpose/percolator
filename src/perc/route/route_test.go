b2076eb379fbe4f5e5b8b19437b20fd7a5f6c97d
Brian W. Wolter <brian@brianwolter.com>
2018-02-11 14:43:47 -0500

    Adding route tests

---
+++ src/perc/route/route_test.go
@@ -0,0 +1,29 @@
+package route
+
+import (
+  "fmt"
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestParseBackend(t *testing.T) {
+  testParseBackend(t, `hello (a='Value')`, "hello", map[string]string{"a": "Value"}, nil)
+  testParseBackend(t, `hello ( key_name = 'Value' )`, "hello", map[string]string{"key_name": "Value"}, nil)
+  testParseBackend(t, `hello/123:456 (key_name='Value\nhere')`, "hello/123:456", map[string]string{"key_name": "Value\nhere"}, nil)
+  testParseBackend(t, `hello/123:456 (key_name='Value', another='Check it')`, "hello/123:456", map[string]string{"key_name": "Value", "another": "Check it"}, nil)
+  testParseBackend(t, `hello/123:456 ( key_name='Value', another='Check it' )`, "hello/123:456", map[string]string{"key_name": "Value", "another": "Check it"}, nil)
+  testParseBackend(t, `hello/123:456 ( key_name='Value', another='Check it' ) trailing stuff, which we ignore`, "hello/123:456", map[string]string{"key_name": "Value", "another": "Check it"}, nil)
+}
+
+func testParseBackend(t *testing.T, in, en string, ep map[string]string, eerr error) bool {
+  b, aerr := parseBackend(in)
+  if aerr != nil || eerr != nil {
+    fmt.Printf("%v -> %v\n", in, aerr)
+    return assert.Equal(t, eerr, aerr, "Errors do not match")
+  }
+  fmt.Printf("%v -> [%v] %v\n", in, b.Name, b.Params)
+  res := true
+  res = res && assert.Equal(t, en, b.Name, "Names do not match")
+  res = res && assert.Equal(t, ep, b.Params, "Params do not match")
+  return res
+}
